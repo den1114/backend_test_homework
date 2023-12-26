dbfd7cb763691b67f3cb9ef60f5ab2aaf827990f
den1114 <154811231+den1114@users.noreply.github.com>
2023-12-26 23:16:38 +0300

    xsxsxsxsxsxsxsxs

--- test_program.py
+++ test_program.py
@@ -1,8 +1,10 @@
 import os
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-dir_files = [filename.lower() for filename in os.listdir(BASE_DIR)]
-   
+
+
+
+dir_files = [filename.lower() for filename in os.listdir(BA
 files_list = ['program.py', 'readme.md']
 
 
